6272af8ab3e680d21f47dd3411f823063deb13ad
Jessie Keck <jkeck@stanford.edu>
2014-02-27 12:40:02 -0800

    Swapped comment examples on sanitize and redact methods

--- lib/classified-ipsum.rb
+++ lib/classified-ipsum.rb
@@ -5,13 +5,13 @@ class String
   DEFAULT_SANITIZATION = '█'
 
   # Returns the redacted string e.g.
-  # "Lee Reilly" becomes "███ ██████"
+  # "Lee Reilly" becomes "[REDACTED] [REDACTED]"
   def redact
     ("#{DEFAULT_REDACTION} " * self.split(" ").size).strip
   end
 
   # Returns the redacted string e.g.
-  # "Lee Reilly" becomes "[REDACTED] [REDACTED]"
+  # "Lee Reilly" becomes "███ ██████"
   def sanitize
     self.downcase.gsub(/[0-9a-z]/, DEFAULT_SANITIZATION)
   end
